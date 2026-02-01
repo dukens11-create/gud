a28beb5531e069d34ab568ba30dee2693d8ae835
Dukens <dukens11@gmail.com>
2026-02-01 14:13:10 -0800

    Reverted lib/routes.dart to original working version from commit e4aecb8

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,17 +1,15 @@
-import 'package:your_project/screens/admin_home.dart';
-import 'package:your_project/screens/manage_drivers_screen.dart';
-import 'package:your_project/screens/create_load_screen.dart';
-import 'package:your_project/screens/admin_load_detail.dart';
-import 'package:your_project/screens/driver_home.dart';
-import 'package:your_project/screens/driver_load_detail.dart';
-import 'package:your_project/screens/earnings_screen.dart';
-import 'package:your_project/screens/login_screen.dart';
+part of 'app_routes.dart';
 
-final Map<String, Widget Function(BuildContext)> appRoutes = {
-  '/login': (context) => LoginScreen(),
-  '/admin': (context) => AdminHome(),
-  '/admin/drivers': (context) => ManageDriversScreen(),
-  '/admin/create-load': (context) => CreateLoadScreen(),
-  '/driver': (context) => DriverHome(),
-  '/driver/earnings': (context) => EarningsScreen(),
-};
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  static const String home = '/home';
+  static const String settings = '/settings';
+
+  static Map<String, WidgetBuilder> get routes {
+    return <String, WidgetBuilder>{
+      home: (context) => HomeScreen(),
+      settings: (context) => SettingsScreen(),
+    };
+  }
+}
