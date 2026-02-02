02bb64de5057cde32a14dd4238b05c060c49f238
Dukens <dukens11@gmail.com>
2026-02-01 16:38:32 -0800

    Update routing for admin screens on 2026-02-02

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
-import 'screens/admin/admin_home.dart';
-import 'screens/driver/driver_home.dart';
-import 'screens/driver/earnings_screen.dart';
+import 'package:your_app/screens/manage_drivers_screen.dart';
+import 'package:your_app/screens/create_load_screen.dart';
 
-final Map<String, WidgetBuilder> appRoutes = {
-  '/login': (context) => const LoginScreen(),
-  '/admin': (context) => const AdminHome(),
-  '/driver': (context) => const DriverHome(),
-  '/driver/earnings': (context) => const EarningsScreen(),
-};
+class Routes {
+  static const String manageDrivers = '/admin/drivers';
+  static const String createLoad = '/admin/create-load';
+  
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case manageDrivers:
+        return MaterialPageRoute(builder: (_) => ManageDriversScreen());
+      case createLoad:
+        return MaterialPageRoute(builder: (_) => CreateLoadScreen());
+      default:
+        return MaterialPageRoute(builder: (_) => UnknownScreen());
+    }
+  }
+}
